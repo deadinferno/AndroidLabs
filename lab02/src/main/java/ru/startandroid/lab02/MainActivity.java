41a7d05540f6912afe1d3cb6beacf78736dfc9c7
Vladimir <38225120+deadinferno@users.noreply.github.com>
2018-09-24 18:02:43 +0300

    upd

--- lab02/src/main/java/ru/startandroid/lab02/MainActivity.java
+++ lab02/src/main/java/ru/startandroid/lab02/MainActivity.java
@@ -2,6 +2,11 @@ package ru.startandroid.lab02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
